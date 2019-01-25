6014e0afc60c57a4639b6aa207b290167c9fc87b
Jerry Gregoire <jerry@arcadecoffee.com>
2019-01-25 13:19:45 -0600

    Updated validation method

--- biggerbot.py
+++ biggerbot.py
@@ -1,4 +1,7 @@
+import hashlib
+import hmac
 import os
+import requests
 import time
 
 from flask import abort, Flask, jsonify, request
@@ -8,30 +11,45 @@ app = Flask(__name__)
 
 
 def is_request_valid(request):
-    is_token_valid = request.form['token'] == \
-            os.environ['SLACK_VERIFICATION_TOKEN']
-    is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
+    slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
+    timestamp = request.headers['X-Slack-Request-Timestamp']
+    slack_signature = request.headers['X-Slack-Signature']
 
-    return is_token_valid and is_team_id_valid
+    # Check the age of the request and reject if more that 5 minutes old
+    if abs(time.time() - int(timestamp)) > 60 * 5:
+        return
+
+    # This is the recipe described here:
+    # https://api.slack.com/docs/verifying-requests-from-slack
+    sig_basestring = 'v0:' + timestamp + ':' + request.get_data(as_text=True)
+    my_signature = 'v0=' + hmac.new(slack_signing_secret.encode('utf-8'),
+            msg=sig_basestring.encode('utf-8'), digestmod='sha256').hexdigest()
+    return hmac.compare_digest(my_signature, slack_signature)
 
 
-@app.route('/hello-there', methods=['POST'])
-def hello_there():
+@app.route('/', methods=['GET', 'POST'])
+def default():
+    return 'ok'
+
+
+@app.route('/ping', methods=['POST'])
+def ping():
     if not is_request_valid(request):
         abort(400)
 
     return jsonify(
-            response_type='in_channel',
-            text='<https://youtu.be/frszEJb0aOo|General Kenobi!>',
+            response_type='ephemeral',
+            text='pong',
             )
 
 
-@app.route('/gmt', methods=['POST'])
-def gmt():
+@app.route('/lunch', methods=['POST'])
+def lunch():
     if not is_request_valid(request):
         abort(400)
 
     return jsonify(
             response_type='in_channel',
-            text='Current GMT is: ' + time.asctime(time.gmtime()),
+            text='Today\'s Lunch:\n```\n' + \
+                    requests.get(os.environ['LUNCH_REQUEST_URL']).text + '```',
             )
